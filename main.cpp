7a7ff1f5486d5d42533fde31ece1c5646cbf63be
0uts1de <www.yusukekaji@gmail.com>
2019-02-18 23:42:03 +0900

    arr to vec

--- main.cpp
+++ main.cpp
@@ -11,21 +11,21 @@ using namespace std;
 using ll = long long int;
 
 template <typename T>
-T cin_arr(vector<T> &arr) {
-    for (auto &i : arr) {
+T cin_vec(vector<T> &vec) {
+    for (auto &i : vec) {
         cin >> i;
     }
     return 0;
 }
 
 template <typename T>
-T cout_arr(vector<T> arr, bool insertNewLine = true) {
+T cout_vec(vector<T> vec, bool insertNewLine = true) {
     if (insertNewLine) {
-        for (const auto &i : arr) {
+        for (const auto &i : vec) {
             cout << i << endl;
         }
     } else {
-        for (const auto &i : arr) {
+        for (const auto &i : vec) {
             cout << i << flush;
         }
     }
@@ -33,16 +33,16 @@ T cout_arr(vector<T> arr, bool insertNewLine = true) {
 }
 
 template <typename T>
-ll min_arr(std::vector<T> arr) {
-    auto iter = min_element(arr.begin(), arr.end());
-    ll index = distance(arr.begin(), iter);
+ll min_vec(std::vector<T> vec) {
+    auto iter = min_element(vec.begin(), vec.end());
+    ll index = distance(vec.begin(), iter);
     return index;
 }
 
 template <typename T>
-ll max_arr(std::vector<T> arr) {
-    auto iter = max_element(arr.begin(), arr.end());
-    ll index = distance(arr.begin(), iter);
+ll max_vec(std::vector<T> vec) {
+    auto iter = max_element(vec.begin(), vec.end());
+    ll index = distance(vec.begin(), iter);
     return index;
 }
 
@@ -50,8 +50,8 @@ int main() {
     int n;
     cin >> n;
     vector<double> a(n);
-    cin_arr(a);
-    cout_arr(a);
-    cout << "max:" << a.at(max_arr(a)) << endl;
+    cin_vec(a);
+    cout_vec(a);
+    cout << "max:" << a.at(max_vec(a)) << endl;
     return 0;
 }
